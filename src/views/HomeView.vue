f0ed15c9bc41a6d4820995d889fe89a38844ecfc
mathensousaa <mhenrique.sousa43@gmail.com>
2025-02-08 17:36:30 -0300

    project structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="w-full h-svh flex items-center justify-center">
+    <h1>Vue + Biome + Tailwind + Radix</h1>
+  </main>
+</template>
